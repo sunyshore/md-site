227bd178dcc9a903457228231423bb168f16cacd
me/rcy <intp@dutchmail.com>
2025-09-07 15:09:34 +0200

    make loader dynamic

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -2,7 +2,20 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch('./components/sidebar.html')
         .then(response => {
             if (!response.ok) {
-                throw new Error('Network response was not ok');
+                fetch('../../components/sidebar.html')
+                    .then(response => {
+                        if (!response.ok) {
+                            throw new Error('Network response was not ok');
+                        }
+                        return response.text();
+                    })
+                    .then(data => {
+                        document.getElementById('sidebar').innerHTML = data;
+                        // var parser = new DOMParser();
+                        // var doc = parser.parseFromString(html, "text/html");
+                    })
+                    .catch(error => console.error('Error loading sidebar:', error));
+                // throw new Error('Network response was not ok');
             }
             return response.text();
         })
